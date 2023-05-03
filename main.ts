46846a5169554dd25126f27749547e85b28ba8c5
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2023-05-03 12:55:23 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -18,3 +18,7 @@ input.onButtonPressed(Button.AB, function () {
     PCAmotor.MotorStop(PCAmotor.Motors.M1)
 })
 
+//měli naprogramovanou lištu. řídit to ručně to bylo složitější. Měli jsme dané kužely. Věděli předem že je radius 30cm. trénovali pohyby.zatoč doprava, dolegva, a rovně.
+//
+//
+//Použití posílání jednoho čísla (64bit) max. 8 jendobytovejch čísel nebo 4 2bytový atd. (asi 2 nebo 16 soustava)
