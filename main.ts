f4e57d503957e6e61b35dce5c21fb03c6a1ce5b0
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2023-05-10 22:17:12 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,52 @@
-let leftI = 0
-let rightI = 0
-const speeds = [-100, -250,]
-basic.forever(function() {
-
-
-
-    
-})
-
-console.log(String.fromCharCode(64))
-console.log("@\dfjdg".charCodeAt(0))
-console.log("@\dfjdg".charCodeAt(1))
-
-
-
-input.onButtonPressed(Button.A, function() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, speeds[++leftI % speeds.length])
-})
-	
-    
-input.onButtonPressed(Button.B, function() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, speeds[++rightI % speeds.length])
-})
-
-input.onButtonPressed(Button.AB, function () {
-    PCAmotor.MotorStop(PCAmotor.Motors.M1)
-})
-
-//měli naprogramovanou lištu. řídit to ručně to bylo složitější. Měli jsme dané kužely. Věděli předem že je radius 30cm. trénovali pohyby.zatoč doprava, dolegva, a rovně.
-//
-//
-//Použití posílání jednoho čísla (64bit) max. 8 jendobytovejch čísel nebo 4 2bytový atd. (asi 2 nebo 16 soustava)
-
-// v pravidelnych intervalech nacitat data z axcelometru
-// kdyz pracujes z rosahy, extremy se neschodujou ??? co posunout interval do kladných čísel
-
-
-function pack(x: number, y: number, z: number): number
-{
-    let xmod = x+1024 //posun intervalu do kladných čísel
-    xmod = Math.floor(xmod / 8) //redukce intervalu (haha)
-    String.fromCharCode(64)
-    "@/asdaf".charCodeAt(1)
-    return 0
-}
 
 radio.setGroup(213)
 radio.setFrequencyBand(7)
 
 
+const speed = 250
 
-radio.onReceivedNumber(function(receivedNumber: number){
-    basic.showNumber(receivedNumber)
-    if (receivedNumber == 1) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speeds[++rightI % speeds.length])
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speeds[++leftI % speeds.length])
-    
-    
-    }
-    if (receivedNumber == 2){
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -speeds[rightI % speeds.length])
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -speeds[leftI % speeds.length])
+const directionState = {
+    STOP: 0,
+    FORWARD: -1,
+    BACKWARD: 1,
+    LEFT: -2,
+    RIGHT: 2
+}
 
-    }
-    if (receivedNumber == 3) {
+let Xdir = directionState.STOP
+let Zdir = directionState.STOP
+
+basic.forever(function () {
+    if (Xdir != directionState.STOP) {
+        if (Xdir == directionState.LEFT) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4,  speed)
+        } else if (Xdir == directionState.RIGHT) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1,  speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, -speed)
+        }
+    } else if (Zdir != directionState.STOP) {
+        if (Zdir == directionState.FORWARD) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, -speed)
+        } else if (Zdir == directionState.BACKWARD) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+        }
+    } else {
         PCAmotor.MotorStop(PCAmotor.Motors.M1)
         PCAmotor.MotorStop(PCAmotor.Motors.M4)
     }
-
 })
+
+radio.onReceivedValue(function (name: string, value: number) {
+    if (name == "Xdir") {
+        Xdir = value
+    }
+    if (name == "Zdir") {
+        Zdir = value
+    }
+})
+
+
+
